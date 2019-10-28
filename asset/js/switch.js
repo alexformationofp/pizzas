396110518ce22dc40d7c1e496b0af686ae3993bc
Milena <milena.wachtel@yahoo.fr>
2019-10-28 11:23:50 +0100

    modif

--- asset/js/switch.js
+++ asset/js/switch.js
@@ -2,11 +2,18 @@ function switchContainer() {
     if ($(window).width() < 992) {
         $(".switch-container").removeClass("container");
         $(".switch-container").addClass("container-fluid");
+        $("#story").css("background", "../img/fonndd768.jpg");
     } else {
         $(".switch-container").addClass("container");
         $(".switch-container").removeClass("container-fluid");
+        $("#story").css("background", "../img/fonndd.jpg");
+    };
+    if ($(window).width () < 768) {
+        $("#story").css("background", "url('./asset/img/fonndd1024.jpg') no-repeat top right / cover");
+    } else {
+        $("#story").css("background", "url('./asset/img/fonndd.jpg') no-repeat top right / cover");
     }
-};
+}; 
 
 switchContainer();
 
